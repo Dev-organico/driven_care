a2e3055a86f24fda9f18c9a83c59cc95c03ef114
Dev-organico <joao.zolet@gmail.com>
2023-04-03 18:35:07 -0300

    fixFeats: get, confirm and cancel appointments from medics and patients

--- src/schemas/appointmentSchema.js
+++ src/schemas/appointmentSchema.js
@@ -4,7 +4,6 @@ import JoiDate from '@joi/date';
 const JoiExtended = joi.extend(JoiDate);
 
 export const appointmentSchema = joi.object({
-  patient_id: joi.number().required(),
   medic_id: joi.number().required(),
   date: JoiExtended.date().format('DD-MM-YY').utc().required(),
   start_time: JoiExtended.date().format('HH-mm').required()
